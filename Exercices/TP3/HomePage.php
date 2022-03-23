7f28383878de0939dd7da5f8ba4f0f934414107c
Ali <Ali.Ghammaz@etu.univ-grenoble-alpes.fr>
2022-03-23 16:09:33 +0100

    ajout la page principale du TMDB

---
+++ Exercices/TP3/HomePage.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <title>TMDB Homepage</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  </head>
+  <style>
+    .centrer {
+        text-align: center;
+    }
+    </style>
+  <body>
+    <h1 style="text-align:center">
+    <b>Welcome to The Movie Database</b>
+    </h1>
+    <p  class="centrer">
+      <img src="https://miro.medium.com/max/800/1*Y9-6_bh5a00rJWWoQ28NMQ.jpeg" />
+    </p>
+
+    <nav style="text-align:center">
+      <a href="formulaire.html">MOVIES</a>
+      &nbsp &nbsp &nbsp &nbsp
+      <a href="traitement2.php">COLLECTION</a>
+      &nbsp &nbsp &nbsp &nbsp
+      <a href="traitement3.php">ACTORS OF A COLLECTION</a>
+      &nbsp &nbsp &nbsp &nbsp
+      <a href="traitement4.php">ROLE OF ACTORS</a>
+  </nav>
+
+  </body>
+</html>
