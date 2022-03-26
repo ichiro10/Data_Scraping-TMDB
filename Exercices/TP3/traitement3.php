71a2a699a1cd2305f1b566dfb0901cf70cd87d76
Ali <Ali.Ghammaz@etu.univ-grenoble-alpes.fr>
2022-03-26 15:40:28 +0100

    Affichage de tout les acteurs de la collection :'Lord of the Rings' avec leur roles et nombre de films dans la collection

---
+++ Exercices/TP3/traitement3.php
@@ -0,0 +1,42 @@
+<?php 
+require_once("functions.php");
+?>
+<!DOCTYPE HTML>
+
+<html lang="fr">
+    <head>
+        <link rel="stylesheet" type="text/css" href="formulaire.css" />
+        <title>Actors</title>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        
+    </head>
+    <style>
+        .centrer {
+        text-align: center;
+         }
+    </style>
+    <body>
+    <h1 style="text-align:center">
+    <b>Welcome to The Movie Database</b>
+    </h1>
+    <p  class="centrer">
+      <img src="https://miro.medium.com/max/800/1*Y9-6_bh5a00rJWWoQ28NMQ.jpeg" />
+    </p>
+
+    <nav style="text-align:center">
+      <a href="formulaire.html">MOVIES</a>
+      &nbsp &nbsp &nbsp &nbsp
+      <a href="traitement2.php">COLLECTION</a>
+      &nbsp &nbsp &nbsp &nbsp
+      <a href="traitement3.php">ACTORS OF A COLLECTION</a>
+      &nbsp &nbsp &nbsp &nbsp
+      <a href="traitement4.php">ROLE OF ACTORS</a>
+  </nav>
+        <?php 
+            echo '<div class="table">';
+            to_HTML_actors(get_actors());
+            echo '</div>';
+     ?>
+    </body>
+</html>
