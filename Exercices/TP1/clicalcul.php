0efcb36b8599f908687f6b65b6aafc281f68f147
Ali <Ali.Ghammaz@etu.univ-grenoble-alpes.fr>
2022-03-29 11:27:40 +0200

    Version finale

--- Exercices/TP1/clicalcul.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'libcalcul.php';
-echo cumul($argv[1], $argv[2], $argv[3]) . "€\n";
-?>
--- Exercices/TP1/libcalcul.php
+++
@@ -1,6 +0,0 @@
-<?php
-function cumul(float $somme , float $taux , float $durée){
-     $cumul= $somme * pow((1 + $taux/100) , $durée);
-
-    return $cumul;
-}
